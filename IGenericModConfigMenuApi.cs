850091b5bb307f171969299f26bc4100e841924c
SheilaEXE <sheila.ariane94@outlook.com>
2026-09-05 16:33:15 -0300

    Fix Generic Mod Config Menu registration

--- IGenericModConfigMenuApi.cs
+++ IGenericModConfigMenuApi.cs
@@ -5,7 +5,7 @@ using System;
 namespace HomeSweetHomeSpouseDialogues
 {
     /// <summary>Minimal API surface used when Generic Mod Config Menu is installed.</summary>
-    internal interface IGenericModConfigMenuApi
+    public interface IGenericModConfigMenuApi
     {
         void Register(IManifest mod, Action reset, Action save, bool titleScreenOnly = false);
 
